4320866dee8849bc88823cb7d2c179b2c7aff905
mashino satoshi <mashino.satoshi@gmail.com>
2019-08-17 17:53:37 +0900

    cookieを受け取れるまで

--- README.md
+++ README.md
@@ -6,10 +6,15 @@ https://qiita.com/shizuma/items/2b2f873a0034839e47ce
 
 ## npmをインストール
 https://qiita.com/taiponrock/items/9001ae194571feb63a5e
-node init
 
 ## expressを使うのだ
 https://expressjs.com/
 
+## cookie-parser
+https://www.utali.io/entry/2016/10/05/031358
+
 ## 起動コマンド
 node app.js
+
+## 実際にcurlを叩いてみる
+curl.exe http://localhost:3000 -H 'Cookie: foo=6; bar=28; baz=496'
--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express')
-const app = express()
+const cookieParser = require('cookie-parser')
 
+const app = express();
+
+app.use(cookieParser())
 app.get('/', (req, res) => {
     console.log('get access.');
+    console.log('--------body--------');
+    console.log(req.body);
+    console.log('--------cookies--------');
+    console.log(req.cookies);
+    console.log('--------headers--------');
+    console.log(req.headers);
+    console.log('--------headers.cookie--------');
+    console.log(req.headers.cookie);
     res.send('Hello World!');
-})
+});
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
