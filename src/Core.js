27c8c52fb1400fb50e69a351384fc5a9e70acb21
mashino satoshi <mashino.satoshi@gmail.com>
2019-09-01 15:53:15 +0900

    singleton

---
+++ src/Core.js
@@ -0,0 +1,21 @@
+class Core {
+    constructor() {
+        if (typeof Core.instance === "object") {
+            console.log("Instance is already created");
+            return Core.instance;
+        }
+        
+        console.log("Create new instance");
+        this.number = String(Math.random());
+        Core.instance = this;
+    }
+
+    get() {
+        return this.number;
+    }
+    set() {
+        this.number = String(Math.random());
+    }
+}
+
+export default new Core();
--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
-const express = require('express')
-const cookieParser = require('cookie-parser')
+import exec1 from "./exec1";
+import exec2 from "./exec2";
+import exec3 from "./exec3";
+import express from "express";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(cookieParser())
-app.get('/', (req, res) => {
+app.get('/cookie', (req, res) => {
     console.log('get access.');
     console.log('--------body--------');
     console.log(req.body);
@@ -14,7 +17,17 @@ app.get('/', (req, res) => {
     console.log(req.headers);
     console.log('--------headers.cookie--------');
     console.log(req.headers.cookie);
-    res.send('Hello World!');
+    res.send('Hello cookie!');
+});
+
+app.get('/singleton/read', (req, res) => {
+    exec1();
+    exec2();
+    res.send('Hello singleton!');
+});
+app.get('/singleton/change', (req, res) => {
+    exec3();
+    res.send('set singleton!');
 });
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
