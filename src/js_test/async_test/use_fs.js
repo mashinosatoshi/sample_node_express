832f483bd3a3a0333ca0878e8009962348127de6
mashino satoshi <mashino.satoshi@gmail.com>
2019-09-12 23:09:16 +0900

    非同期処理を作れないぃぃぃぃぃぃぃ

--- src/js_test/async_test/use_fs.js
+++ src/js_test/async_test/use_fs.js
@@ -1,16 +1,19 @@
 const exec = function () {
     const fs = require('fs');
-
-    // 読み込みが完了したときの処理
-    const listener = function (err, data) {
-        console.log(data);
-    }
     console.log(process.argv[1]);
     // 非同期で読み込む
-    fs.readFile('C:\\git\\sample_node_express\\lib\\foo.txt',{ encoding: 'utf8' },listener
+    fs.readFile(
+        'C:\\git\\sample_node_express\\lib\\foo.txt',
+        { encoding: 'utf8' },
+        listener
     );
 
     console.log("finished");
+}
+
+// 読み込みが完了したときの処理
+const listener = function (err, data) {
+    console.log(data);
 }
 
 //export default exec;
